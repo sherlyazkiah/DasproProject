a15dcc1b3f97f3cedcd8564ccea1972f418d42c3
sherlyazkiah <sherlyazkiah12@gmail.com>
2023-10-06 15:20:19 +0700

    add payment method

--- EmployeePayrollSystem.java
+++ EmployeePayrollSystem.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
    
 public class EmployeePayrollSystem {
   public static void main(String args[]){ 
-            
     Scanner sc = new Scanner(System.in);
     System.out.println("              Employee Payroll              ");
     System.out.println("============================================");
      
-    String name;
-    int category;
-    int salaryPerHour;
-    int workHours, basicSalary, overtime,  tip = 20000, bonus, salary, allowence = 50000, workPeriod;
+    String name, payMethod;
+    int workPeriod, category, workHours, salaryPerHour, basicSalary, overtime,  tip = 10000, bonus, salary, allowance = 50000;
      
     System.out.print("Name: ");
     name = sc.nextLine();
     System.out.print("Work Period: ");
-    workPeriod = sc.nextLine();
+    workPeriod = sc.nextInt();
     System.out.print("Category: ");
     category = sc.nextInt();
     System.out.print("Work Hours: ");
@@ -27,13 +24,48 @@ public class EmployeePayrollSystem {
 
     basicSalary = workHours*salaryPerHour;
     bonus = overtime*tip;
-    salary = basicSalary + bonus + allowence;
+    salary = basicSalary + bonus + allowance;
 
     System.out.println("============================================");
     System.out.println("Basic Salary: " + basicSalary);
     System.out.println("Bonus: " + bonus);
     System.out.println("Allowance: " + allowance);
     System.out.println("Salary: " + salary);
+    System.out.println("");
 
+    sc.nextLine();
+
+    System.out.print("Payment Method(BNI/BRI/Bank Jatim/Mandiri/BCA): ");
+    payMethod = sc.nextLine();
+
+    switch(payMethod) {
+      case "BNI" :
+          System.out.print("Input BNI account number(009): ");
+          int BNI = sc.nextInt();
+          System.out.println("Salary is currently being processed for transfer to the account...");
+          break;
+      case "BRI":
+          System.out.print("Input BRI account number(002): ");
+          int BRI = sc.nextInt();
+          System.out.println("Salary is currently being processed for transfer to the account...");
+          break;
+      case "Bank Jatim":
+          System.out.print("Input Bank Jatim account number(114): ");
+          int bankJatim = sc.nextInt();
+          System.out.println("Salary is currently being processed for transfer to the account...");
+          break;
+      case "Mandiri":
+          System.out.print("Input Mandiri account number(008): ");
+          int mandiri = sc.nextInt();
+          System.out.println("Salary is currently being processed for transfer to the account...");
+          break;
+      case "BCA":
+          System.out.print("Input BCA account number(014): ");
+          int BCA = sc.nextInt();
+          System.out.println("Salary is currently being processed for transfer to the account...");
+          break;
+      default:
+          System.out.println("Input invalid");
+    }
   }
 }
